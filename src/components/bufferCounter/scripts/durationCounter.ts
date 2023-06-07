8bfc2b63c49d27441f4e63b02ccae0ef891c80f9
neverFeltAlive <20This.Is.My.Mail00@proronmail.com>
2023-06-07 23:40:48 +0300

    [feature] - fixed counters

--- src/components/bufferCounter/scripts/durationCounter.ts
+++ src/components/bufferCounter/scripts/durationCounter.ts
@@ -1,15 +1,25 @@
 import Hls from 'hls.js';
 
+const getBufferLength = (ranges: TimeRanges | undefined) => {
+  if (!ranges) return 0;
+
+  let length = 0;
+  for (let i = 0; i < ranges.length; i++) {
+    length += ranges.end(i) - ranges.start(i);
+  }
+
+  return length;
+};
+
 export const addDurationCounterEvents = (hls: Hls) => {
   const durationCounter = document.querySelector<HTMLSpanElement>(
     '.js-buffer-length-count'
   );
 
   if (durationCounter) {
-    hls.on(Hls.Events.BUFFER_APPENDED, (event: string, data) => {
-      const chunkDuration = data.frag.duration;
-      durationCounter.innerText = (
-        +durationCounter.innerText + chunkDuration
+    hls.on(Hls.Events.BUFFER_APPENDED, () => {
+      durationCounter.innerText = getBufferLength(
+        hls.media?.buffered
       ).toLocaleString();
     });
   }
--- src/components/bufferCounter/scripts/sizeCounter.ts
+++ src/components/bufferCounter/scripts/sizeCounter.ts
@@ -7,15 +7,12 @@ export const addSizeCounterEvents = (hls: Hls) => {
     '.js-buffer-size-count'
   );
 
-  let currentSize = 0;
-
   if (sizeCounter) {
     hls.on(
       Hls.Events.BUFFER_APPENDED,
       (event: string, data: BufferAppendedData) => {
         const chunkSize = data.chunkMeta.size;
-        currentSize += chunkSize;
-        sizeCounter.innerText = bytesToSizeString(currentSize);
+        sizeCounter.innerText = bytesToSizeString(chunkSize);
       }
     );
   }
