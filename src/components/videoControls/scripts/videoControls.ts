2b41ff9f78d57d0ed0ccbb1983f4afb64f37a7e5
neverFeltAlive <20This.Is.My.Mail00@proronmail.com>
2023-06-08 18:49:33 +0300

    [feature] - refactored component

--- src/components/videoControls/scripts/videoControls.ts
+++ src/components/videoControls/scripts/videoControls.ts
@@ -1,38 +1,12 @@
 import '../styles/style.scss';
+import { addFullscreenControls } from './fulllscreenControls.ts';
+import { addPlayStateControls } from './playStateControls.ts';
 
 export const init = (videoElement: HTMLVideoElement) => {
   const controls = document.querySelector('.js-controls');
 
   if (controls) {
-    const playButton = document.querySelector('.js-controls-play');
-    if (playButton) {
-      playButton.addEventListener('click', () => {
-        videoElement.play();
-        controls.classList.add('active');
-      });
-    }
-
-    const pauseButton = document.querySelector('.js-controls-pause');
-    if (pauseButton) {
-      pauseButton.addEventListener('click', () => {
-        videoElement.pause();
-        controls.classList.remove('active');
-      });
-    }
-
-    const fullscreen = document.querySelector('.js-controls-fullscreen');
-    const modal = document.querySelector<HTMLDialogElement>('#video-modal');
-    if (fullscreen && modal) {
-      fullscreen.addEventListener('click', (event) => {
-        event.stopPropagation();
-        const container = modal.classList.contains('active')
-          ? document.querySelector('.js-video-container')
-          : modal.querySelector('.js-modal-container');
-        if (container) {
-          container.appendChild(controls);
-          modal.classList.toggle('active');
-        }
-      });
-    }
+    addPlayStateControls(controls, videoElement);
+    addFullscreenControls(controls);
   }
 };
