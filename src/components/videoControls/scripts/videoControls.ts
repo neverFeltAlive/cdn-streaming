b14dfe0c60753b308f0b7348f83f43f5dbb18567
neverFeltAlive <20This.Is.My.Mail00@proronmail.com>
2023-06-08 19:37:20 +0300

    [feature] - added progress bar functionality

--- src/components/videoControls/scripts/videoControls.ts
+++ src/components/videoControls/scripts/videoControls.ts
@@ -1,12 +1,19 @@
+import Hls from 'hls.js';
+
 import '../styles/style.scss';
 import { addFullscreenControls } from './fulllscreenControls.ts';
 import { addPlayStateControls } from './playStateControls.ts';
+import { addProgressControls } from './progressControls.ts';
 
-export const init = (videoElement: HTMLVideoElement) => {
+export const init = (videoElement: HTMLVideoElement, hls: Hls) => {
   const controls = document.querySelector('.js-controls');
 
   if (controls) {
     addPlayStateControls(controls, videoElement);
     addFullscreenControls(controls);
+
+    hls.on(Hls.Events.LEVEL_LOADED, () => {
+      addProgressControls(videoElement);
+    });
   }
 };
--- src/components/videoPlayer/scripts/videoController.ts
+++
@@ -1,3 +0,0 @@
-export const handleTimeUpdate = (event: Event) => {
-  console.log(event);
-};
