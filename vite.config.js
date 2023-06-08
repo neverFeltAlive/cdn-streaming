07338cf56386a47261e7cfb891b5b38e5d9d3404
neverFeltAlive <20This.Is.My.Mail00@proronmail.com>
2023-06-08 19:54:12 +0300

    [release] - deployment config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig(() => ({
+  base: '/cdn-streaming/',
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
